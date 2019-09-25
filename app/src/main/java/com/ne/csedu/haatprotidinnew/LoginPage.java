5a735a3c72903b7eff6f4f347fce3d94a44419ae
sayem7906 <sh.sayem.haque36@gmail.com>
2019-09-25 23:11:50 +0600

    Initial commit

---
+++ app/src/main/java/com/ne/csedu/haatprotidinnew/LoginPage.java
@@ -0,0 +1,14 @@
+package com.ne.csedu.haatprotidinnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+}
